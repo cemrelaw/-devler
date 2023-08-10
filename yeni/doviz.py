c4d877a582506570ac6d7461c2c89d84d636bb91
cemrelaw <111667114+cemrelaw@users.noreply.github.com>
2023-08-10 23:38:07 +0300

    yeni

---
+++ yeni/doviz.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+url1 = "https://kur.doviz.com"
+page = requests.get(url1)
+soup = BeautifulSoup(page.content, "html.parser")
+
+
+doviz = input("hesaplamak istediğiniz döviz kurunu giriniz:")    
+miktar = int(input("kur hesaplamak istediğiniz miktarı giriniz: "))
+
+def kur_hesapla(): 
+    deger = soup.find_all("span", {"data-socket-key": doviz})
+    deger = deger[0].text
+    deger = deger.replace(",", ".")
+    sonuc = miktar * float(deger)
+    print(f"{doviz}: {deger} \n{miktar} {doviz}: {sonuc} TRY")
+
+kur_hesapla()
